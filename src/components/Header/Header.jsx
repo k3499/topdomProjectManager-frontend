e14863589e6a68fa153ce7723bcdb86eafb2e3ad
k3499 <k3499@yandex.ru>
2024-01-24 14:19:00 +0300

    avatar block

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import './Header.css';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { Avatar } from "antd";
+import "./Header.css";
 
 const Header = () => {
   return (
-   <header className="header">
-    <Link  to="/" className="logo">logo</Link>
-    
-    <NavLink to='/cian' className="menu__item menu__item_cian">Cian</NavLink>
-    <NavLink to='/direct' className="menu__item menu__item_direct" >Direct</NavLink>
-    <NavLink to='/avito' className="menu__item menu__item_avito">Avito</NavLink>
-   </header>
+    <header className="header">
+      <Link to="/" className="logo">
+        logo
+      </Link>
+
+      <NavLink to="/cian" className="menu__item menu__item_cian">
+        Cian
+      </NavLink>
+      <NavLink to="/direct" className="menu__item menu__item_direct">
+        Direct
+      </NavLink>
+      <NavLink to="/avito" className="menu__item menu__item_avito">
+        Avito
+      </NavLink>
+      <Link to="/" className="avatar">
+        <Avatar
+          style={{ backgroundColor: "#1b1b1b", verticalAlign: "middle" }}
+          size={50}
+          gap={3}
+        >
+          К Д
+        </Avatar>
+      </Link>
+    </header>
   );
 };
 
